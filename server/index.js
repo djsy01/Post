1fc6e1c4dfc79acc37835038446b995f14aabe2b
djsy01 <dlsgh8884@naver.com>
2025-06-10 11:08:00 +0900

    배포오류 수정

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   origin: ['http://localhost:5173', 'https://post-psi-two.vercel.app'],  // React 개발 서버 주소 (배포시 변경 필요)
   credentials: true
